01f108c56790a7c0c2de1578a661f67c49b566dc
GideoneClementino <gideonedeeon@hotmail.com>
2021-10-11 16:20:49 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/SpringBootJPA/entities/User.java
+++ src/main/java/com/educandoweb/SpringBootJPA/entities/User.java
@@ -1,9 +1,20 @@
 package com.educandoweb.SpringBootJPA.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
