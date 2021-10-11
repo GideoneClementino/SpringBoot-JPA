eb2c79a21879f4713428e14406287edd8d926aae
GideoneClementino <gideonedeeon@hotmail.com>
2021-10-11 18:51:26 -0300

    Service layer, e registro de componentes

---
+++ src/main/java/com/educandoweb/SpringBootJPA/services/UserService.java
@@ -0,0 +1,24 @@
+package com.educandoweb.SpringBootJPA.services;
+
+import com.educandoweb.SpringBootJPA.entities.User;
+import com.educandoweb.SpringBootJPA.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
